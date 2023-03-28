179f23c8651679ec66cc08124902f2256bfb5747
shaikgouserabbani <skrab3006@gmail.com>
2023-03-28 17:49:38 +0530

    hirehub

--- routes/notification.js
+++ routes/notification.js
@@ -4,7 +4,7 @@ let router = express.Router();
 
 let Notifications = require('../models/noti_DB');
 
-router.get('/notification', async function(req, res) {
+router.get('/notifications', async function(req, res) {
     try {
         let allnotifs = await Notifications.find({});
         res.render('index-notif.ejs',{allnotifs});
@@ -14,28 +14,28 @@ router.get('/notification', async function(req, res) {
     }
 });
 
-router.get('/notification/new', function(req, res){
+router.get('/notifications/new', function(req, res){
     res.render('new-notif.ejs');
 
 });
 
-router.post('/notification', async function(req, res) {
+router.post('/notifications', async function(req, res) {
     try {
         let notif = new Notifications({
             body:req.body.body,
             author: req.body.author,
         });
         await notif.save();
-        res.redirect('/notification');
+        res.redirect('/notifications');
 
     } catch (error) {
         console.error(error);
     }
 });
 
-router.delete('/notification/:id', async function(req, res) {
+router.delete('/notifications/:id', async function(req, res) {
     Notifications.findByIdAndDelete(req.params.id);
-    res.redirect('/notification');
+    res.redirect('/notifications');
 });
 
 module.exports = router;
